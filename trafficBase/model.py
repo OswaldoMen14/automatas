60be38d3b4b74f460de765672ce9fa02e91b3ae2
Alank99 <93668758+Alank99@users.noreply.github.com>
2023-11-22 13:39:37 -0600

    avances en movimiento de los autos

--- trafficBase/model.py
+++ trafficBase/model.py
@@ -1,6 +1,7 @@
 from mesa import Model
 from mesa.time import RandomActivation
 from mesa.space import MultiGrid
+import random
 from agent import *
 import json
 
@@ -17,6 +18,8 @@ class CityModel(Model):
         dataDictionary = json.load(open("city_files/mapDictionary.json"))
 
         self.traffic_lights = []
+
+        self.destination = []
 
         # Load the map file. The map file is a text file where each character represents an agent.
         with open('city_files/2022_base.txt') as baseFile:
@@ -47,10 +50,18 @@ class CityModel(Model):
                     elif col == "D":
                         agent = Destination(f"d_{r*self.width+c}", self)
                         self.grid.place_agent(agent, (c, self.height - r - 1))
+                        self.destination.append((c, self.height - r - 1))
 
         self.num_agents = N
         self.running = True
         self.step_count = 0
+
+        def remove_car(self, agent):
+            """
+            Remove a car from the model.
+            """
+            self.schedule.remove(agent) # Remove the agent from the scheduler
+            self.grid.remove_agent(agent) # Remove the agent from the grid
 
     def step(self):
         '''Advance the model by one step.'''
@@ -58,22 +69,38 @@ class CityModel(Model):
         self.step_count += 1
 
         if self.step_count == 1 or self.step_count % 10 == 0:
-            car1 = Car(f"car_{self.step_count}_1", self)
+            destination = random.choice(self.destination)
+            car1 = Car(f"car_{self.step_count}_1", self, (0, self.height-1), destination)
             self.grid.place_agent(car1, (0, self.height - 1))
+            car1.initialize_path()  # Initialize the path after placing the car
             self.schedule.add(car1)
+            
             print("new car added to the grid")
+            print("this car is going to: ", destination)
 
-            car2 = Car(f"car_{self.step_count}_2", self)
+
+
+            destination = random.choice(self.destination)
+            car2 = Car(f"car_{self.step_count}_2", self,(self.width - 1, self.height - 1) , destination)
             self.grid.place_agent(car2, (self.width - 1, self.height - 1))
+            car2.initialize_path()  # Initialize the path after placing the car
             self.schedule.add(car2)
             print("new car added to the grid")
+            print("this car is going to: ", destination)
 
-            car3 = Car(f"car_{self.step_count}_3", self)
+            
+            destination = random.choice(self.destination)
+            car3 = Car(f"car_{self.step_count}_3", self, (0, 0) ,destination)
             self.grid.place_agent(car3, (0, 0))
+            car3.initialize_path()  # Initialize the path after placing the car
             self.schedule.add(car3)
             print("new car added to the grid")
+            print("this car is going to: ", destination)
 
-            car4 = Car(f"car_{self.step_count}_4", self)
+            destination = random.choice(self.destination)
+            car4 = Car(f"car_{self.step_count}_4", self,(self.width - 1, 0) , destination)
             self.grid.place_agent(car4, (self.width - 1, 0))
+            car4.initialize_path()  # Initialize the path after placing the car
             self.schedule.add(car4)
             print("new car added to the grid")
+            print("this car is going to: ", destination)
